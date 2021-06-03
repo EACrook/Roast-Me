a9e15ceb2793c3519c3a65108c2d109623cf5282
Emily Crook <Emily.A.Crook96@gmail.com>
2021-06-03 18:49:10 -0500

    scaled down to two tables with data

---
+++ seeds/drinks-seed.js
@@ -0,0 +1,17 @@
+const drinksData = [{
+    drink_name: 'Cold Brew',
+    recipe:
+},
+{
+    drink_name: 'Latte',
+    recipe:
+},
+{
+    drink_name: 'Iced Coffee',
+    recipe:
+},
+{
+    drink_name: 'Espresso',
+    recipe:
+}
+];
---
+++ seeds/ingredients-seed.js
@@ -0,0 +1,94 @@
+const ingredientData = [{
+        ingredient_name: 'Light Roast',
+    },
+    {
+        ingredient_name: 'Medium Roast',
+    },
+    {
+        ingredient_name: 'Dark Roast',
+    },
+    {
+        ingredient_name: 'Decaf'
+    }, 
+    {
+        ingredient_name: 'Espresso'
+    },
+    {
+        ingredient_name: '2% Milk',
+    },
+    {
+        ingredient_name: 'Whole Milk',
+    },
+    {
+        ingredient_name: '1% Milk',
+    },
+    {
+        ingredient_name: 'Half & Half',
+    },
+    {
+        ingredient_name: 'Heavy Whipping Cream',
+    },
+    {
+        ingredient_name: 'Oat Milk'
+    },
+    {
+        ingredient_name: 'Almond Milk'
+    },
+    {
+        ingredient_name: 'Soy Milk'
+    },
+    {
+        ingredient_name: 'Sugar',
+    },
+    {
+        ingredient_name: 'Raw Cane Sugar',
+    },
+    {
+        ingredient_name: 'Brown Sugar',
+    },
+    {
+        ingredient_name: 'Stevia',
+    },
+    {
+        ingredient_name: 'Honey',
+    },
+    {
+        ingredient_name: 'Mocha/chocolate',
+    },
+    {
+        ingredient_name: 'Hazelnut',
+    },
+    {
+        ingredient_name: 'Vanilla',
+    },
+    {
+        ingredient_name: 'Liquid Sugar',
+    },
+    {
+        ingredient_name: 'Peppermint'
+    },
+    {
+        ingredient_name: 'White Mocha'
+    },
+    {
+        ingredient_name: 'Toffee'
+    },
+    {
+        ingredient_name: 'Green Tea',
+    },
+    {
+        ingredient_name: 'Black Tea',
+    }, 
+    {
+        ingredient_name: 'Herbal Tea',
+    }, 
+    {
+        ingredient_name: 'Oolong Tea',
+    }, 
+    {
+        ingredient_name: 'Rooibos Tea'
+    }, 
+    {
+        ingredient_name: 'White Tea'
+    }
+];
