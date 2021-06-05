11679563fc0e6b479f59db5a88877293d8db1c36
Emily Crook <Emily.A.Crook96@gmail.com>
2021-06-04 19:53:58 -0500

    all tables finished

--- seeds/drink-ingredients-seeds.js
+++ seeds/drink-ingredients-seeds.js
@@ -5,6 +5,118 @@ const drinkingredientData = [
     {
         drink_id: 1,
         ingredient_id: 5
+    },
+    {
+        drink_id: 1,
+        ingredient_id: 14
+    },
+    {
+        drink_id: 2,
+        ingredient_id: 5
+    },
+    {
+        drink_id: 2,
+        ingredient_id: 14
+    },
+    {
+        drink_id: 2,
+        ingredient_id: 35
+    },
+    {
+        drink_id: 2,
+        ingredient_id: 58
+    },
+    {
+        drink_id: 3,
+        ingredient_id: 5
+    },
+    {
+        drink_id: 3,
+        ingredient_id: 59
+    },
+    {
+        drink_id: 4,
+        ingredient_id: 14
+    },
+    {
+        drink_id: 4,
+        ingredient_id: 35
+    },
+    {
+        drink_id: 5,
+        ingredient_id: 5
+    },
+    {
+        drink_id: 5,
+        ingredient_id: 22
+    },
+    {
+        drink_id: 6,
+        ingredient_id: 1
+    },
+    {
+        drink_id: 6,
+        ingredient_id: 2
+    },
+    {
+        drink_id: 6,
+        ingredient_id: 3
+    },
+    {
+        drink_id: 6,
+        ingredient_id: 4
+    },
+    {
+        drink_id: 7,
+        ingredient_id: 50
+    },
+    {
+        drink_id: 7,
+        ingredient_id: 51
+    },
+    {
+        drink_id: 7,
+        ingredient_id: 52
+    },
+    {
+        drink_id: 7,
+        ingredient_id: 53
+    },
+    {
+        drink_id: 7,
+        ingredient_id: 54
+    },
+    {
+        drink_id: 7,
+        ingredient_id: 55
+    },
+    {
+        drink_id: 7,
+        ingredient_id: 56
+    },
+    {
+        drink_id: 7,
+        ingredient_id: 57
+    },
+    {
+        drink_id: 8,
+        ingredient_id: 57
+    },
+    {
+        drink_id: 8,
+        ingredient_id: 14
+    },
+    {
+        drink_id: 9,
+        ingredient_id: 56
+    },
+    {
+        drink_id: 9,
+        ingredient_id: 59
+    },
+    {
+        drink_id: 9,
+        ingredient_id: 14
     }
 ];
 
--- seeds/drinks-seed.js
+++ seeds/drinks-seed.js
@@ -7,6 +7,34 @@ const drinksData = [{
 {
     drink_name: 'Mocha',
     temp: 'hot'
+},
+{
+    drink_name: 'Americano',
+    temp: 'hot'
+},
+{
+   drink_name: 'Hot Chocolate',
+   temp: 'hot' 
+},
+{
+    drink_name: 'Cappuccino',
+    temp: 'hot'
+},
+{
+    drink_name: 'Brewed Coffee',
+    temp: 'hot'
+},
+{
+    drink_name: 'Hot Tea',
+    temp: 'hot'
+},
+{
+    drink_name: 'Green Tea Latte',
+    temp: 'hot'
+},
+{
+    drink_name: 'Chai Tea Latte',
+    temp: 'hot'
 }
 ];
 
