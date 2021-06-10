51dfd257e032fbb7e6786aa0fab7384c0fc443a7
katredford <katharine.redford@gmail.com>
2021-06-09 23:18:13 -0500

    salting password

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 #Project 2
 
+install:
+
+npm init
+
+npm install express sequelize mysql2
+
+npm install dotenv
+
+npm install bcrypt
+
+npm install express-handlebars
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -33,21 +34,22 @@ User.init(
         }
       }
     },
+    {
+      hooks: {
+        beforeCreate(userData){
+          return bcrypt.hash(userData.password, 10).then(newUserData => {
+            return newUserData
+          });
+        }
   
-  {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
-  }
-);
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
+  
 
 module.exports = User;
