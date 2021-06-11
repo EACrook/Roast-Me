a8ac84f7141786361dbd51d44f9e3735ddd49bd8
katredford <katharine.redford@gmail.com>
2021-06-10 21:01:57 -0500

    save hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const { beforeCreate, beforeValidate, beforeUpdate } = require('./Drink');
 
 // create our User model
 class User extends Model {}
@@ -36,13 +37,16 @@ User.init(
     },
     {
       hooks: {
-        beforeCreate(userData){
-          return bcrypt.hash(userData.password, 10).then(newUserData => {
-            return newUserData
-          });
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData; 
+        },
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
         }
-  
       },
+
       sequelize,
       timestamps: false,
       freezeTableName: true,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,7 @@ const {User} = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     User.findAll({
-        attributes: {exclude: ['password']}
+        // attributes: {exclude: ['password']}
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -51,6 +51,7 @@ router.post('/', (req, res) => {
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id:req.params.id
         }
