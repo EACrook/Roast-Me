0f4b2dbe1cd18651c0dcee609356a0b9b4577bb7
Emily Crook <Emily.A.Crook96@gmail.com>
2021-06-12 18:38:38 -0500

    user and post routes linked

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,21 +1,15 @@
 const router = require('express').Router();
 const drinkRoutes = require('./drink-routes');
 const ingredientRoutes = require('./ingredient-routes');
-<<<<<<< HEAD:routes/api/index.js
-const userRoutes =require('./user-routes.js');
+const userRoutes =require('./user-routes');
 const postRoutes = require('./post-routes');
+const insultRoutes = require('./insult-routes');
 
 
 router.use('/drinks', drinkRoutes);
 router.use('/ingredients', ingredientRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-=======
-const insultRoutes = require('./insult-routes');
-
-router.use('/drinks', drinkRoutes);
-router.use('/ingredients', ingredientRoutes);
 router.use('/insults', insultRoutes);
->>>>>>> 31b51c7a7b0a406b0dd3cb48a0ef58cd7dcfeecf:controllers/api/index.js
 
 module.exports = router;
