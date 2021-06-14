1b1c5f2f186067a44feb1d5dcbfce75ffe40e09e
Emily Crook <Emily.A.Crook96@gmail.com>
2021-06-14 18:50:51 -0500

    post middleware is set up to take info from the posts

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -15,81 +15,107 @@ const {
 const apiRoutes = require('../controllers');
 
 router.get('/', (req, res) => {
-    res.render('home', {
-        homeHeader: 'home-header',
-        homeRoastMe: 'home-roast-me'
+    let data = {
+        posts: '',
+        username: ''
+    }
+    Post.findAll({
+        attributes: [
+            'id',
+            'comment',
+            'photo_url'
+        ],
+        include: [{
+            model: User,
+            attributes: ['username']
+        }]
+    }).then(dbHomeData => {
+        console.log('POSTS', dbHomeData);
+        const posts = dbHomeData.map(post => post.get({
+            plain: true
+        }));
+        data.posts = posts;
+        res.render('home', {
+            homeHeader: 'home-header',
+            homeRoastMe: 'home-roast-me'
+        })
     })
+    .catch(err => {
+        console.log('err', err);
+        res.status(500).json(err);
+    })
+
 })
 
 router.get('/blackboard', (req, res) => {
-        let data = {
+    let data = {
         drinks: '',
         ingredients: '',
         blackboardBody: 'blackboard'
     }
     // Pull all drinks to display
     Drink.findAll({
-        attributes: [
-            'id',
-            'drink_name',
-            'temp',
-            'ingredient_id'
-        ],
-        include: [{
-            model: Ingredient,
-                attributes: ['ingredient_name'],
+            attributes: [
+                'id',
+                'drink_name',
+                'temp',
+                'ingredient_id'
+            ],
+            include: [{
+                    model: Ingredient,
+                    attributes: ['ingredient_name'],
                     // include: {
                     //     model: DrinkIngredient,
                     //     attributes: ['drink_id', 'ingredient_id']
                     // }
-            },
+                },
                 // {
                 //     model: DrinkIngredient,
                 //     attributes: ['drink_id', 'ingredient_id']
                 // }
-        ]
-    }).then(dbDrinkData => {
-        // console.log('DRINKS', dbDrinkData.ingredients);
-
-        const drinks = dbDrinkData.map(drink => drink.get({
-            plain: true
-        }));
-        data.drinks = drinks;
-
-        // console.log('drinksplain', drinks)
-        Ingredient.findAll({
-            attributes: [
-                'id',
-                'ingredient_name'
             ]
-        }).then(dbIngredientData => {
-            // console.log('ingredients', dbIngredientData)
-            const ingredients = dbIngredientData.map(ingredient => ingredient.get({
+        }).then(dbDrinkData => {
+            // console.log('DRINKS', dbDrinkData.ingredients);
+
+            const drinks = dbDrinkData.map(drink => drink.get({
                 plain: true
             }));
-            data.ingredients = ingredients;
+            data.drinks = drinks;
 
-            Insult.findAll({
+            // console.log('drinksplain', drinks)
+            Ingredient.findAll({
                 attributes: [
                     'id',
-                    'insult'
+                    'ingredient_name'
                 ]
-            }).then(dbInsultData => {
-                const insults = dbInsultData.map(insults => insults.get({
+            }).then(dbIngredientData => {
+                // console.log('ingredients', dbIngredientData)
+                const ingredients = dbIngredientData.map(ingredient => ingredient.get({
                     plain: true
                 }));
-                let insultLength = insults.length
+                data.ingredients = ingredients;
 
-                let idx = Math.floor(Math.random()*insultLength)
-                    
-                data.insults = insults[idx];
-                // console.log('data.insults length', data.insults)
-                // console.log('data', data)
-                res.render('blackboard', data);
+                Insult.findAll({
+                    attributes: [
+                        'id',
+                        'insult'
+                    ]
+                }).then(dbInsultData => {
+                    const insults = dbInsultData.map(insults => insults.get({
+                        plain: true
+                    }));
+                    let insultLength = insults.length
+
+                    let idx = Math.floor(Math.random() * insultLength)
+
+                    data.insults = insults[idx];
+                    // console.log('data.insults length', data.insults)
+                    // console.log('data', data)
+                    res.render('blackboard', data);
+                })
             })
-        })
 
-    })
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -99,29 +125,27 @@ router.get('/blackboard', (req, res) => {
 router.get('/edit-caption', (req, res) => {
     // action: get ingredients with sequelize then pass into view {ingredients}
     Post.findAll({
-        attributes: [
-            'id',
-            'comment',
-            'photo_url'
-        ],
-        include: [
-            {
+            attributes: [
+                'id',
+                'comment',
+                'photo_url'
+            ],
+            include: [{
                 model: User,
                 attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostdata => {
-        console.log('abut to send page!!!', dbPostdata)
-        res.render('edit-caption', {
-            pageHeader: 'page-header',
-            pageRoastMe: 'page-roast-me'
+            }]
         })
-    })
-    .catch(err => {
-        console.log('err', err);
-        res.status(500).json(err);
-    })
+        .then(dbPostdata => {
+            console.log('abut to send page!!!', dbPostdata)
+            res.render('edit-caption', {
+                pageHeader: 'page-header',
+                pageRoastMe: 'page-roast-me'
+            })
+        })
+        .catch(err => {
+            console.log('err', err);
+            res.status(500).json(err);
+        })
 
 })
 
