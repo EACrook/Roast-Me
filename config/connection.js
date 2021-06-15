a11b6ab8974312a0889cdf35846e6dba43be2eb2
Becca Wyland <rwyland@uwalumni.com>
2021-06-15 14:36:05 -0500

    db config for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+let sequelize;
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+}
 
 module.exports = sequelize;
